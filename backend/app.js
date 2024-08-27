49476bdf0344b41cc2c5efb88aa2c3a38073bdb1
theubaidahmed <theubaidahmed@gmail.com>
2024-08-27 20:19:53 +0530

    env updated and .htaccess added

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(morgan('dev'));
 
 app.use(
     cors({
-        origin: ['http://localhost:5173'],
+        origin: ['http://localhost:5173', 'https://heliverse-6w7r.onrender.com'],
     })
 );
 
